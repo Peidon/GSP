0c096fb6203b3532940504a09ec27246b403fc4f
Peidon <32288910+Peidon@users.noreply.github.com>
2018-03-13 23:26:16 +0800

    Update Test.java
    改了每行的数据个数为2～10之间的随机数，忘记改支持度阈值。

--- data-mining/src/com/test/Test.java
+++ data-mining/src/com/test/Test.java
@@ -7,7 +7,7 @@ public class Test {
 	public static void main(String[] args) {
 		 String filePath = Filepath.test;  
 	     Long t = System.currentTimeMillis();
-	     int minSupportCount = 100; //最小支持度阈值
+	     int minSupportCount = 8; //最小支持度阈值
 	     Apriori_SP test = new Apriori_SP(filePath,minSupportCount);
 	     test.generate();
 	     System.out.println("It spent " + (System.currentTimeMillis() - t) + " Miller Seconds!");
